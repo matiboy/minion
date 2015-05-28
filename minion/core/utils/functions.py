9c564cddb839487aeebcd79d99cf88efadcb29ce
matiboy <mathieu.chauvinc@gmail.com>
2015-05-28 08:10:17 +0800

    Decorator for configuration getter

--- minion/core/utils/functions.py
+++ minion/core/utils/functions.py
@@ -3,3 +3,17 @@ import functools
 
 def compose(*functions):
     return functools.reduce(lambda f, g: lambda x: f(g(x)), functions)
+
+
+def configuration_getter(default_function):
+    """
+    Decorator to avoid all the get_configuration methods
+    Use _get_<property_name> or get_<property_name> as name of the decorated method
+    The decorated method will be used to determine the default value if `property_name` is not present in the configuration dict
+    """
+    _, _, property_name = default_function.__name__.partition('get_')
+
+    def fn(self):
+        return self.get_configuration(property_name, default_function(self))
+
+    return fn
