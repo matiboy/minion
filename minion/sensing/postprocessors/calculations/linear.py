928a53a2f7988b4df9d92221880f9febffa91d3c
matiboy <mathieu.chauvinc@gmail.com>
2015-09-16 18:59:58 +0800

    Post processor for linear manipulation of value

---
+++ minion/sensing/postprocessors/calculations/linear.py
@@ -0,0 +1,32 @@
+import minion.sensing.postprocessors
+import minion.sensing.exceptions
+import minion.core.components.exceptions
+import multiprocessing
+import minion.core.utils.functions
+
+logger = multiprocessing.get_logger()
+
+
+class LinearPostprocessor(minion.sensing.postprocessors.BasePostprocessor):
+    """
+    Post processor to transform a value into another using a linear function
+    Configuration simply requires the values of a and b in y = ax + b
+    """
+    @minion.core.utils.functions.configuration_getter
+    def _get_a(self):
+        return 1
+
+    @minion.core.utils.functions.configuration_getter
+    def _get_b(self):
+        return 0
+
+    def process(self, data):
+        # Make sure we can read the state as a float
+        try:
+            num = float(data)
+        except ValueError:
+            raise minion.sensing.exceptions.DataReadError('Unable to parse <{}> as a float'.format(data))
+
+        # Apply
+        print self._get_a(), num, self._get_b()
+        return (self._get_a() * num) + self._get_b()
