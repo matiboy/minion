fa809e47970192b81f87b0523da12d95660676e0
matiboy <mathieu.chauvinc@gmail.com>
2015-11-09 09:02:40 +0800

    HTML doc for pyttsx

--- minion/acting/mouth/pyttsx/setup.py
+++ minion/acting/mouth/pyttsx/setup.py
@@ -7,16 +7,14 @@ defines = {
             'class': 'minion.acting.mouth.pyttsx.pyttsx.SimpleSay',
             'default_channel': 'minion:speak',
             'description': '''
-# Pyttsx based Text To Speech
-#
-# Refer to https://pyttsx.readthedocs.org/en/latest/engine.html
-#
-# On Raspberry Pi/Banana Pi etc, make sure Jack service is running by using the shell command jack_control start
+<h3>Pyttsx based Text To Speech</h3>
+<p>Refer to <a href="https://pyttsx.readthedocs.org/en/latest/engine.html">https://pyttsx.readthedocs.org/en/latest/engine.html</a></p>
+<p>On Raspberry Pi/Banana Pi etc, make sure Jack service is running by using the shell command jack_control start</p>
             ''',
             'setup': [
                 {
                     'name': 'voice',
-                    'default': 'localhost',
+                    'default': '',
                     'type': 'input',
                     'message': 'Provide voice id (leave empty for default)'
                 },
